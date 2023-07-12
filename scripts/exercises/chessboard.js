ad5c1b0d41f4445359460c2b3807fa93d60055b0
Tyler Chrestoff <datummine@proton.me>
2023-07-11 20:03:32 -0400

    Chessboard fix

--- scripts/exercises/chessboard.js
+++ scripts/exercises/chessboard.js
@@ -1,8 +1,8 @@
-let size = 10;
+let size = 8;
 let board = "";
 for (let n = 0; n < size; n++) {
   for (let m = 0; m < size; m++) {
-    board += (n + m) % 2 == 0 ? "#" : "@";
+    board += (n + m) % 2 == 0 ? " " : "#";
   }
   board += "\n";
 }
